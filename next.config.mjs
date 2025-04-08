fed3fa765dbf966ddcbdebe206e62729c08ca3a3
Janine-Codes <janinekrse@gmail.com>
2025-04-08 18:33:25 +0200

    Deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  output: "export",
   webpack: (config, { dev }) => {
     if (dev) {
       config.optimization.providedExports = false;
