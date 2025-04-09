de1a9e20dbec26e1fe34154bf9b3c7ca9ea0cc4d
Janine-Codes <janinekrse@gmail.com>
2025-04-09 11:19:37 +0200

    Fix Footer smaller + Mailito link corrected

--- src/app/components/sub-components/button/Button.js
+++ src/app/components/sub-components/button/Button.js
@@ -3,7 +3,7 @@ import styles from "./button.module.css";
 
 export default function Button({ label, variant = "default", onClick }) {
   const handleClick = () => {
-    window.location.href = "mailto:loubrg@gmail.com";
+    window.location.href = "mailto:louhbrg@gmail.com";
     console.log("should open");
   };
 
